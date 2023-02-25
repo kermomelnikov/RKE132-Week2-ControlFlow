8956fe2a1032615390e386b0ca6d94aed1ed50b6
Kermo M <melnikovkermo@gmail.com>
2023-02-25 15:42:23 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,25 @@
+﻿// rakendus küsib kasutajal valida tema sugu (m/f)
+// rakendus küsib kasutaja perekonnanime sisetada
+// lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+// "Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); // loeb konsoolist maha andmeid string ehk sõne formaadis
+
+Console.WriteLine("Please, enter your lastname:");
+
+string userName = Console.ReadLine();
+
+if(userGender == 'm')
+ {
+    Console.WriteLine($"Welcome, Mr. {userName}!");
+ }
+else if(userGender == 'f') 
+{
+    Console.WriteLine($"Welcome, Ms. {userName}!");
+}
+else 
+{
+    Console.WriteLine($"Welcome {userName}!");
+}
---
+++ Week2/Program.cs
@@ -0,0 +1,4 @@
+﻿// Week 2
+Console.WriteLine("Hello, world!");
+// uued andmetüübid: int ja char
+// if-statement (control flow)
